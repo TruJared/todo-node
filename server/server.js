f577cf0a6376c42a89850f28ed4143c9e4eb92a4
Jared Truscott <Jtruscott78@gmail.com>
2018-04-02 16:35:45 -0400

    added GET by ID

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // library modules
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 // mongoose
 const { mongoose } = require('./db/mongoose');
@@ -21,7 +22,27 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find().then(todos => res.send({ todos }), e => res.status(400).send(e));
+  Todo.find().then(todos => res.status(200).send({ todos }), e => res.status(400).send(e));
+});
+
+// GET /todos/${_id}
+
+app.get('/todos/:id', (req, res) => {
+  const { id } = req.params;
+
+  // is ID valid?
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('bad ID');
+  }
+
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.status(200).send({ todo });
+    })
+    .catch(e => res.status(400).send());
 });
 
 app.listen(3000, () => {
